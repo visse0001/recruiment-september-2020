9f3911799475b4639ba288ad458dc708b4b30f2a
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-09-07 01:27:56 +0200

    Path paramethers works fine in FastAPI.

--- fastapiapp/main.py
+++ fastapiapp/main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 # Start applications
@@ -5,3 +7,13 @@ app = FastAPI(
     title='A microservice for data authorization.',
     description='Input: name, email. Output: PASS or FAIL',
 )
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/users/{first_name}/{last_name}/{email}")
+def read_users(first_name: str, last_name: str, email: str):
+    return {f'First name: {first_name}, last name: {last_name}, email: {email}'}
