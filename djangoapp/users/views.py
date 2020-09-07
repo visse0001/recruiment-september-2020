9400d3a6f0221d5453631b4b059ef5dee0f0e690
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-09-07 08:57:35 +0200

    Fix register view, add Login view post and get.

--- djangoapp/users/views.py
+++ djangoapp/users/views.py
@@ -12,26 +12,35 @@ def register(request):
 
             context = {'form': form}
 
-            return render(request, "users/register.html", context)
+            return render(request, "users/login.html", context)
 
     if request.method == "GET":
         form = RegisterForm()
 
-        return render(request, "users/register.html")
+        context = {'form': form}
+
+        return render(request, "users/register.html", context)
 
 
 def login(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # Load the profil
+            user.first_name = form.cleaned_data.get('first_name')
             # user -> authenticate by requests to fastapiapp localhost:8000
             # if response from fastapiapp is 'PASS'
             first_name = request.POST['first_name']
             last_name = request.POST['last_name']
             email = request.POST['email']
 
-            context = {"first_name": first_name, "last_name": last_name, "email": email}
-            return HttpResponse(f'Authenticated successfully! Context: {context}')
+            context = {"first_name": first_name,
+                       "last_name": last_name,
+                       "email": email
+                       }
+
+            return HttpResponse(f'{context}')
         else:
             return HttpResponse('Invalid login.')
 
