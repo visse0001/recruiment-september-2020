5453011283f579a140be6f607a9089708f50a32c
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-09-09 10:48:00 +0200

    Fastapi: Create engine in module db_conn.

--- fastapiapp/db_conn.py
+++ fastapiapp/db_conn.py
@@ -1,12 +1,17 @@
-import databases
 import sqlalchemy
-from fastapi import FastAPI
-from pydantic import BaseModel
+from sqlalchemy import create_engine, databases
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = postgresql+psycopg2://{user}:{password}@{host}:{port}
 DATABASE_URL = "postgresql+psycopg2://postgres:postgres@postgres:5432"
 
 database = databases.Database(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
 
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
