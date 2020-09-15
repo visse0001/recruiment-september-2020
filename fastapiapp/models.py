5b108212ffa9fc86b2e0e57faaf84441aa23d82c
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-09-15 10:49:19 +0200

    FastAPI: Move pydantic model to schemas.

--- fastapiapp/models.py
+++ fastapiapp/models.py
@@ -1,7 +1,19 @@
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
 
+from .database import Base
 
-class Message(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    password = Column(String, unique=True, index=True)
+    last_login = Column(String, index=True)
+    is_superuser = Column(Integer, index=True)
+    username = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, index=True)
+    is_staff = Column(Integer, index=True)
+    is_active = Column(Integer, index=True)
+    date_joined = Column(String, index=True)
+    first_name = Column(String, index=True)
+
