99b2b76e20c24b97abbaa70a3dcf4298676f6841
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-09-17 12:10:00 +0200

    Django: check if resposne is PASS or FAIL.

--- djangoapp/users/views.py
+++ djangoapp/users/views.py
@@ -4,6 +4,7 @@ import requests
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from .forms import RegisterForm
@@ -37,7 +38,10 @@ def user_auth(request):
 
     # context_json = json.dumps(context)
 
+    # FastAPI
     # url = 'http://localhost:8000/check'
+
+    # Flask
     url = 'http://localhost:4000/check'
 
     fastapi_response = requests.post(url, json=context)
@@ -46,4 +50,9 @@ def user_auth(request):
         'response': fastapi_response.text
     }
 
-    return JsonResponse(response, safe=False)
+    resp = response['response']
+    if resp == 'PASS':
+        # all_users = User.objects.all()
+        return HttpResponse('User is in the database.')
+    else:
+        return HttpResponse('User is not in the database.')
